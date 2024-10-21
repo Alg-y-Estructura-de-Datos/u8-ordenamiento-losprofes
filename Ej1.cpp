db54cd9779456f0894c486566e12645794129c05
Matias Tosetti Docente <141670493+matiastosettiOficial@users.noreply.github.com>
2024-10-21 15:28:51 -0300

    Update Ej1.cpp
    se mejora algoritmo bubble sort

--- Ej1.cpp
+++ Ej1.cpp
@@ -8,15 +8,19 @@ using namespace std;
 
 // Función para aplicar Bubble Sort
 unsigned long long bubbleSort(vector<int> &arr) {
+    bool seguir = true;
     unsigned long long contadorComparaciones = 0;
     int n = arr.size();
-    for (int i = 0; i < n - 1; i++) {
+    for (int i = 0; i < n - 1 && seguir; i++) {
+        seguir = false;
         contadorComparaciones++;
 
         for (int j = 0; j < n - i - 1; j++) {
             contadorComparaciones++;
             contadorComparaciones++;
+
             if (arr[j] > arr[j + 1]) {
+                seguir = true;
                 swap(arr[j], arr[j + 1]);
             }
         }
@@ -48,20 +52,20 @@ int main() {
         file.close();
 
         // Mostrar el array antes de ordenar
-//        cout << "Array desordenado: ";
-//        printArray(arr);
+        cout << "Array desordenado: ";
+        //printArray(arr);
 
         // Aplicar Bubble Sort
         unsigned long long cantidadCondicionales  = bubbleSort(arr);
         cout << "Cantidad de comparaciones numeros al Azar: " << cantidadCondicionales << endl;
         // Mostrar el array ordenado
         cout << "Array ordenado: "<< endl;
-//        printArray(arr);
-//
+       // printArray(arr);
+
     } else {
         cout << "No se pudo abrir el archivo." << endl;
     }
-
+    vector<int> arr2;
     ifstream file2("numerosOrdenados.txt");
     if (file2.is_open()) {
         // Leer el archivo y extraer los números
@@ -69,25 +73,25 @@ int main() {
             stringstream ss(line);
             int num;
             ss >> num;
-            arr.push_back(num);
+            arr2.push_back(num);
         }
         file2.close();
 
         // Mostrar el array antes de ordenar
-//        cout << "Array desordenado: ";
-//        printArray(arr);
+        cout << "Array desordenado: ";
+        //printArray(arr2);
 
         // Aplicar Bubble Sort
-        unsigned long long cantidadCondicionales  = bubbleSort(arr);
+        unsigned long long cantidadCondicionales  = bubbleSort(arr2);
         cout << "Cantidad de comparaciones numeros ordenados: " << cantidadCondicionales << endl;
         // Mostrar el array ordenado
         cout << "Array ordenado: "<< endl;
-//        printArray(arr);
-//
+        //printArray(arr2);
+
     } else {
         cout << "No se pudo abrir el archivo." << endl;
     }
-
+    vector<int> arr3;
     ifstream file3("numerosInversa.txt");
     if (file3.is_open()) {
         // Leer el archivo y extraer los números
@@ -95,29 +99,28 @@ int main() {
             stringstream ss(line);
             int num;
             ss >> num;
-            arr.push_back(num);
+            arr3.push_back(num);
         }
         file3.close();
 
         // Mostrar el array antes de ordenar
-//        cout << "Array desordenado: ";
-//        printArray(arr);
+        cout << "Array desordenado: ";
+       // printArray(arr3);
 
         // Aplicar Bubble Sort
-        unsigned long long cantidadCondicionales  = bubbleSort(arr);
-        cout << "Cantidad de comparaciones numeros ordenados: " << cantidadCondicionales << endl;
+        unsigned long long cantidadCondicionales  = bubbleSort(arr3);
+        cout << "Cantidad de comparaciones numeros inversas: " << cantidadCondicionales << endl;
         // Mostrar el array ordenado
-        cout << "Array ordenado: "<< endl;
-//        printArray(arr);
-//
+        cout << "Array inversas: "<< endl;
+        //printArray(arr3);
+
     } else {
         cout << "No se pudo abrir el archivo." << endl;
     }
 
     return 0;
 }
-
-/* (Descomentar para proar segunda parte
+/* //(Descomentar para proar segunda parte
 #include <iostream>
 #include <fstream>
 #include <sstream>
@@ -169,19 +172,23 @@ vector<Partido> leerArchivoCSV(const string& nombreArchivo) {
     archivo.close();
     return partidos;
 }
+
 // Algoritmo de Bubble Sort con contador de comparaciones
 template<typename T, typename Comparator>
 unsigned long long bubbleSort(vector<T>& arr, Comparator comparar) {
     int size = arr.size();
+    bool seguir = true;
     int contadorComparaciones = 0; // Contador de comparaciones
 
-    for (int i = 0; i < size - 1; i++) {
+    for (int i = 0; i < size - 1 && seguir; i++) {
+        seguir=false;
         contadorComparaciones++; // Incrementar contador de comparaciones
         for (int j = 0; j < size - i - 1; j++) {
             contadorComparaciones++; // Incrementar contador de comparaciones
             contadorComparaciones++; // Incrementar contador de comparaciones
             // Usar el comparator para decidir si se debe intercambiar
             if (comparar(arr[j + 1], arr[j])) {
+                seguir=true;
                 swap(arr[j], arr[j + 1]);
             }
         }
@@ -206,17 +213,18 @@ int main() {
         return 1;
     }
 
-//    cout << "Partidos antes de ordenar:\n";
-//    imprimirPartidos(partidos);
+    cout << "Partidos antes de ordenar:\n";
+   // imprimirPartidos(partidos);
 
     unsigned long long condicionales = bubbleSort(partidos, [](const Partido& a, const Partido& b) {
         return a.golesLocal < b.golesLocal;
+//        return a.golesLocal > b.golesLocal;
     });
 
 
     cout << "\nPartidos después de ordenar por goles locales:\n";
     cout << "\nCantidad de condicionales:\n" << condicionales << endl;
-   // imprimirPartidos(partidos);
+    //imprimirPartidos(partidos);
 
    condicionales = bubbleSort(partidos, [](const Partido& a, const Partido& b) {
         return a.golesVisitante < b.golesVisitante;
@@ -228,5 +236,4 @@ int main() {
 
     return 0;
 }
-
 */
